4df98f0424d41ce4f846f930c6f5fb9b2c277153
Onesmus Mutyauvyu <89151741+Onesmus123@users.noreply.github.com>
2025-07-22 05:47:48 +0300

    Updated Shop Page

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,20 +1,33 @@
-import React from 'react'
-import Carousel from '../Components/Carousel/Carousel'
-import Popular from '../Components/Popular/Popular'
-import Offers from '../Components/Offers/Offers'
-import NewCollections from '../Components/NewCollections/NewCollections'
-import NewsLetter from '../Components/NewsLetter/NewsLetter'
+import React from 'react';
+import Carousel from '../Components/Carousel/Carousel';
+import Popular from '../Components/Popular/Popular';
+import Offers from '../Components/Offers/Offers';
+import NewCollections from '../Components/NewCollections/NewCollections';
+import NewsLetter from '../Components/NewsLetter/NewsLetter';
+import './CSS/Shop.css'; // Add this file to manage spacing & layout if needed
 
 const Shop = () => {
   return (
-    <div>
-        <Carousel />
-        <Popular/>
-        <Offers/>
-        <NewCollections/>
-        <NewsLetter/>
-    </div>
-  )
-}
+    <div className="shop-container">
+      <Carousel />
 
-export default Shop
+      <section className="shop-section">
+        <Popular />
+      </section>
+
+      <section className="shop-section">
+        <Offers />
+      </section>
+
+      <section className="shop-section">
+        <NewCollections />
+      </section>
+
+      <section className="shop-section">
+        <NewsLetter />
+      </section>
+    </div>
+  );
+};
+
+export default Shop;
